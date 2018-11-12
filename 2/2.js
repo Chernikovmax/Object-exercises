7019215326056cf9bd9a64740f6d520b8be06790
Chernikovmax <chernik.max@gmail.com>
2018-11-12 15:28:20 +0400

    Exercise - complete

--- 2/2.js
+++ 2/2.js
@@ -1,15 +1,11 @@
 class CylinderVolume {
   constructor(r, h) {
-    this.Pi = Math.Pi;
     this.radiusSquare = r*r;
-    this.higth = h;
+    this.height = h;
   }
-  get CylVolume() {
-    return V = (this.Pi * this.radiusSquare *this.higth).toFixed(4)
-  }
-  set CylVolume(value) {
-
+  getCylinderV() {
+    return (Math.PI * this.radiusSquare *this.height).toFixed(4)
   }
 }
 const someCylinder = new CylinderVolume(4, 12);
-console.log(someCylinder.Pi);
+console.log(someCylinder.getCylinderV());
