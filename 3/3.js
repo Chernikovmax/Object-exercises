40d5ece294bf988288e3109ad38707a56fbd0633
Chernikovmax <chernik.max@gmail.com>
2018-11-14 12:55:08 +0400

    Implemented bubble sort using class.

--- 3/3.js
+++ 3/3.js
@@ -1,14 +1,37 @@
-function bubbleSort(arr) {
-  for (let i = 0; i < arr.length-1; i++) {
-    for (let j = 0; j < arr.length -1 -i; j++) {
-      if (arr[j] > arr[j+1]) {
-        let temp = arr[j];
-        arr[j] = arr[j+1];
-        arr[j+1] = temp;
+class BubbleSort {
+  constructor(arr) {
+    this.array = arr;
+  }
+  numericSort() {
+    for (let i = 0; i < this.array.length-1; i++) {
+      for (let j = 0; j < this.array.length -1 -i; j++) {
+        if (this.array[j] > this.array[j+1]) {
+          let temp = this.array[j];
+          this.array[j] = this.array[j+1];
+          this.array[j+1] = temp;
+        }
       }
     }
+    return this.array;
   }
-  return arr;
 }
 
-console.log(bubbleSort([-2, 0, 1, 3, 4, 6, 2, 8, 17, -50, 9]));
+
+
+let array = new BubbleSort([-2, 0, 1, 3, 4, 6, 2, 8, 17, -50, 9, 13, 13, 16, 12]);
+console.log(array.numericSort());
+
+
+
+// function bubbleSort(arr) {
+//   for (let i = 0; i < arr.length-1; i++) {
+//     for (let j = 0; j < arr.length -1 -i; j++) {
+//       if (arr[j] > arr[j+1]) {
+//         let temp = arr[j];
+//         arr[j] = arr[j+1];
+//         arr[j+1] = temp;
+//       }
+//     }
+//   }
+//   return arr;
+// }
